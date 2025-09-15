5c09bd9f939ad02b5d20367c78357513ca5ee761
thatmossymoo <grace.lafountain3@gmail.com>
2025-09-15 15:01:04 -0400

    Completed Project

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,14 @@
 public class Main
 {
     public static void main(String[] args)
-
+    {
+                double currentBalance = 5000.00;
+                double interestRate = 0.17;
+                double firstInterest = 0.00;
+                double secondInterest = 0.00;
+                firstInterest = currentBalance * interestRate;
+                secondInterest = (currentBalance + firstInterest)*interestRate;
+                System.out.println("Your interest after 1 month is: " + firstInterest);
+                System.out.println("Your interest after 2 months is: " + secondInterest);
     }
 }
